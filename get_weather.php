2ab93d6eab367143702b33fb12ce053ba906d2fc
ex00e0 <148848809+ex00e0@users.noreply.github.com>
2024-11-13 07:38:28 +0300

    Add files via upload

--- get_weather.php
+++ get_weather.php
@@ -1,9 +1,24 @@
 <?php
 // Ваш API-ключ
+
 $apiKey = "15800c3022510d25a95fc0112c100499";
 
-// ID города Уфа (у OpenWeatherMap у каждого города есть свой ID)
-$cityId = "479561"; // ID для Уфы
+$get = $_GET['name'];
+if ($get == 1) {
+    $cityId = "479561";
+}
+else if ($get == 2) {
+    $cityId = "524901";
+}
+else if ($get == 3) {
+    $cityId = "554840";
+}
+else if ($get == 4) {
+    $cityId = "2023469";
+}
+else if ($get == 5) {
+    $cityId = "1489425";
+}
 
 // Формируем URL для запроса к API OpenWeatherMap
 $url = "http://api.openweathermap.org/data/2.5/weather?id={$cityId}&units=metric&lang=ru&appid={$apiKey}";
